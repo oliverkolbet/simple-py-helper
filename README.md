d42b8106a207e8dc625a5d0cbd4dc00cf170f925
oliverkolbet <oliver.kolbet@gmail.com>
2020-07-03 11:00:20 -0700

    modified:   README.md 	modified:   infolog.txt 	modified:   startup.py

--- README.md
+++ README.md
@@ -3,8 +3,9 @@ A python-based project for the command line containing many different tools
 
 ## tools include: 
 * smtp emailing
-   * *works for yahoo, gmail, hotmail, etc.*
+   * *works for yahoo, gmail, hotmail, aruba*
 * saving to text files
+* customized personal info
 * custom aliases for the helper **or** the shell.
 
 Made for rPi originally.
--- startup.py
+++ startup.py
@@ -1,1 +1,32 @@
 #file is a placeholder while other files are being made.
+from time import sleep
+
+name = ''
+email = ''
+nick = ''
+bio = ''
+prov = ''
+
+print('Hello! welcome to version 1 of the python helperBot.')
+sleep(0.6)
+print('What is your name?')
+name = input('>')
+sleep(0.6)
+print('Hello, '+name+'!')
+sleep(0.7)
+print('What would you like me to call you?')
+nick = input('>')
+sleep(0.6)
+print('We need your email for our email service.')
+print('What is your email?')
+email = input('>')
+print('We also need the name of your provider.i.e, Gmail, Hotmail, Yahoo, etc')
+prov = input('>').lower()
+print('Thanks! Now, could you tell us a little about yourself?')
+bio = input('>')
+print('Alright! finishing up some things and then your helper will be ready!')
+info = [name, '\n', email, '\n', nick, '\n', bio, '\n', prov]
+attfile = open('infolog.txt','w')
+attfile.writelines(info)
+sleep(3)
+print('Done!')
